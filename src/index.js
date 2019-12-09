41bbb6cf4ee36d284eb9a4d1a2ca81ecf5cbb3ab
SEULGI YOON <sgyoon.info@gmail.com>
2019-12-09 22:18:02 +0900

    불필요한 파일 삭제 및 코드 초기화

--- src/index.js
+++ src/index.js
@@ -1,12 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const HelloWorld = () => (
+  <div>HELLO WORLD</div>
+);
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+ReactDOM.render(<HelloWorld />, document.getElementById("root"));
