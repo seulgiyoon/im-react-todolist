fa85348b62f2702029d4af35fa7aa0afc534017b
SEULGI YOON <seulgiyoon@SEULGIui-MacBookPro.local>
2020-03-07 20:07:36 +0900

    Updates

---
+++ precache-manifest.6ff11c8f061ba58aa7f5d31bca0cd569.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c05ad603ce3ab383480311b633b29b64",
+    "url": "/im-react-todolist/index.html"
+  },
+  {
+    "revision": "b7b2adc75ed5ac582302",
+    "url": "/im-react-todolist/static/js/2.81b9acd9.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/im-react-todolist/static/js/2.81b9acd9.chunk.js.LICENSE"
+  },
+  {
+    "revision": "6bcaff22b5b7fd1134e2",
+    "url": "/im-react-todolist/static/js/main.2114700d.chunk.js"
+  },
+  {
+    "revision": "053f7027d2dba6886426",
+    "url": "/im-react-todolist/static/js/runtime-main.cb9d326f.js"
+  }
+]);
